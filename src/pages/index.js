1c76557fef318624f596abf9cc1554f2afe433c1
Adrian Nuyda <adamnuyda@gmail.com>
2020-02-05 00:27:20 -0800

    adds react hooks to render pokemon from api

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,55 @@
-import React from "react"
-import { Link } from "gatsby"
+import React, { useState, useEffect } from "react";
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+import axios from 'axios';
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const IndexPage = () => {
+
+  const [cool, setPokemon] = useState([]);
+
+  const numOfPokemon = 151;
+  let pokemonIds = [];
+
+  for (let i = 1; i <= numOfPokemon; i++) {
+    pokemonIds.push(i);
+  }
+
+  const fetchPokemon = async () => {
+    const pokemons = pokemonIds.map(id => {
+      return axios.get(`http://pokeapi.co/api/v2/pokemon/${id}`).then(res => res.data);
+    });
+
+    Promise.all(pokemons).then((res) => {
+      setPokemon(res);
+    });
+  };
+
+  useEffect(() => {
+    fetchPokemon();
+  }, []);
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <ul>
+        {
+          cool.map((pokemon, index) => {
+            return <li id={index} key={index}>
+              <h3>{pokemon.name}</h3>
+              <ul className="types">
+                <li>
+                  {pokemon.types[0].type.name}
+                </li>
+              </ul>
+              <img src={pokemon.sprites.front_default} alt={pokemon.title} />
+            </li>
+          })
+        }
+      </ul>
+    </Layout>
+  );
+
+};
 
 export default IndexPage
