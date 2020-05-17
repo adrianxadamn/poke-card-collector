bb46ffa64eda2fbaa5d47c105b4ac101fb6d8645
Adrian Nuyda <adamnuyda@gmail.com>
2020-05-17 12:21:58 -0700

    checks to see if window is defined

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -6,8 +6,10 @@
 
 // You can delete this file if you're not using it
 
-const React = require('react');
-const Layout = require('./src/components/layout').default
+if (typeof window !== `undefined`) {
+  const React = require('react');
+  const Layout = require('./src/components/layout').default
+}
 
 exports.wrapPageElement = ({element, props}) => {
 	return <Layout {...props}>{element}</Layout>
